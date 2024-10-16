54a081dfab20278a1a82c0e0b0de1ce8bfae73a1
OdionOseiwe <oseiweifebhor@gmail.com>
2024-10-16 17:24:51 +0100

    fixed HOST URL in authstore.js

--- frontend/src/store/authstore.js
+++ frontend/src/store/authstore.js
@@ -1,7 +1,7 @@
 import {create} from "zustand"
 import axios from 'axios'
 
-const HOST_URL = import.meta.env.MODE === "development" ?"http://localhost:5000/api/auth" : "api/auth"
+const HOST_URL = process.env.NODE_ENV === "development" ?"http://localhost:5000/api/auth" : "/api/auth"
 
 axios.defaults.withCredentials = true;
 
